236ba9879e65e3d91e5c671eb4dc4ccf41995ce4
sliang <shink2010@gmail.com>
2022-01-15 10:01:03 +0800

    Initial commit

---
+++ src/main/java/org/shi/code/reading/main/Main.java
@@ -0,0 +1,12 @@
+package org.shi.code.reading.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:test.xml");
+
+//        context.getBean("")
+    }
+}
