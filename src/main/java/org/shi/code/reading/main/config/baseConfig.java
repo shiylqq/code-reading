833766899816b0c087b60db014a315e771d83911
sliang <shink2010@gmail.com>
2022-01-26 00:33:59 +0800

    增加注解支持

---
+++ src/main/java/org/shi/code/reading/main/config/baseConfig.java
@@ -0,0 +1,8 @@
+package org.shi.code.reading.main.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class baseConfig {
+
+}
---
+++ src/main/java/org/shi/code/reading/main/service/PersonService.java
@@ -0,0 +1,7 @@
+package org.shi.code.reading.main.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonService {
+}
