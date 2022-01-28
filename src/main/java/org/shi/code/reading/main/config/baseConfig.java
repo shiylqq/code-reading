c654dc37ea3d3a73364c9fd00c1d223f10210af0
史亮 <shiliang@goo.team>
2022-01-28 16:20:50 +0800

    placeholder support

--- src/main/java/org/shi/code/reading/main/config/baseConfig.java
+++ src/main/java/org/shi/code/reading/main/config/baseConfig.java
@@ -1,8 +1,17 @@
 package org.shi.code.reading.main.config;
 
+import org.shi.code.reading.main.pojo.Man;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class baseConfig {
+
+    @Bean
+    public Man man(){
+        Man man = new Man();
+        man.setJj(true);
+        return man;
+    }
 
 }
---
+++ src/main/java/org/shi/code/reading/main/pojo/Man.java
@@ -0,0 +1,19 @@
+package org.shi.code.reading.main.pojo;
+
+/**
+ * @author shiliang
+ * @version 1.0
+ * @date 2022/1/28
+ **/
+public class Man extends Person{
+    private Boolean jj;
+
+
+    public Boolean getJj() {
+        return jj;
+    }
+
+    public void setJj(Boolean jj) {
+        this.jj = jj;
+    }
+}
