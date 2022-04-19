598d7b42ef2df1abe1f0d36ac3c5ee4f8472020f
sliang <shink2010@gmail.com>
2022-04-19 16:25:01 +0800

    自动注入测试

--- src/main/java/org/shi/code/reading/autoWired/pojo/Person.java
+++ src/main/java/org/shi/code/reading/autoWired/pojo/Person.java
@@ -2,11 +2,13 @@ package org.shi.code.reading.autoWired.pojo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.annotation.Resource;
+
 public class Person {
     private String name;
 
-    @Autowired
-    private Pen pen;
+    @Resource
+    private Pen pen1;
 
     public String getName() {
         return name;
@@ -16,19 +18,19 @@ public class Person {
         this.name = name;
     }
 
-    public Pen getPen() {
-        return pen;
+    public Pen getPen1() {
+        return pen1;
     }
 
-    public void setPen(Pen pen) {
-        this.pen = pen;
+    public void setPen1(Pen pen1) {
+        this.pen1 = pen1;
     }
 
     @Override
     public String toString() {
         return "Person{" +
                 "name='" + name + '\'' +
-                ", pen=" + pen +
+                ", pen=" + pen1 +
                 '}';
     }
 }
